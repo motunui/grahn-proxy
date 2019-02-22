bf1c866ea9c0b113ab32d90dbd9f91edec77d55d
Grahn Cooledge <gcooled29@gmail.com>
2019-02-21 22:12:20 -0500

    Render Paths To All

--- server/server.js
+++ server/server.js
@@ -6,14 +6,16 @@ const morgan = require('morgan');
 const server = express();
 
 server.use(morgan('dev'));
-server.use(express.static(path.join(__dirname, '../public')));
+// server.use(express.static(path.join(__dirname, '../public')));
 
 server.disable('x-powered-by');
+
+server.all('/', express.static(path.join(__dirname, '../public')));
 
 server.listen(config.PORT, () => {
   console.log(`Listening on port ${config.PORT}`);
 });
 
-server.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+// server.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../public/index.html'));
+// });
